b1e6468571b5c177f5a76545b9d90b291e5564f1
Auxence <audel22@student.sdu.dk>
2023-01-16 13:41:29 +0100

    Revolt of the TextViews

--- app/src/main/java/fr/isen/deleuziere/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/deleuziere/androiderestaurant/HomeActivity.kt
@@ -3,18 +3,28 @@ package fr.isen.deleuziere.androiderestaurant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val action = findViewById<Button>(R.id.PlatsButton)
-        action.setOnClickListener{
-            Toast.makeText(this, "Hello ISEN M1", Toast.LENGTH_SHORT).show()
-            Snackbar.make(it, "Hello ISEN M1", Snackbar.LENGTH_SHORT).show()
+        val action1 = findViewById<TextView>(R.id.PlatsButton)
+        action1.setOnClickListener{
+            Toast.makeText(this, "Chargement des entrées", Toast.LENGTH_SHORT).show()
+            //Snackbar.make(it, "Hello ISEN M1", Snackbar.LENGTH_SHORT).show()
+        }
+
+        val action2 = findViewById<TextView>(R.id.EntreesButton)
+        action2.setOnClickListener{
+            Toast.makeText(this, "Chargement des plats", Toast.LENGTH_SHORT).show()
+        }
+
+        val action3 = findViewById<TextView>(R.id.DessertsButton)
+        action3.setOnClickListener{
+            Toast.makeText(this, "Chargement des desserts", Toast.LENGTH_SHORT).show()
         }
     }
 }
